3a7ad338ce999d5d5273193bd7416247be5bec8b
Lucas Souza <42921327+lucas2179@users.noreply.github.com>
2023-02-03 11:35:49 -0300

    Update main.py

--- main.py
+++ main.py
@@ -62,7 +62,7 @@ async def test(request):
     with tracer.span(name="teste") as span:
         conn = await connect_to_db()
         cursor = conn.cursor()
-        query = 
+        
         cursor.execute('SELECT * from "IMP_TEST"."ISO_COUNTRY"')
         rows = cursor.fetchall()
         logger.exception("here is your log")
